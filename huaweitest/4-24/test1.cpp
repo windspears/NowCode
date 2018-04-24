c0ce591a5d57ee8347a180807eef05423f56c95f
wind <windspears@163.com>
2018-04-24 16:01:36 +0800

    modify the bug

--- huaweitest/4-24/test1.cpp
+++ huaweitest/4-24/test1.cpp
@@ -11,14 +11,14 @@ int main()
 		{
 			goto NG;
 		}
-		for(int k=0;k<s.length();k++)
+		for(int k=0;k<s.length();++k)
 		{
 			if(s[k]>='a'&&s[k]<='z'){Downletter=1;}
 			else if(s[k]>='0'&&s[k]<='9'){flagnumber=1;}
-			else if(s[k]>='A'&&s[k]<='Z'){Downletter=1;}
+			else if(s[k]>='A'&&s[k]<='Z'){Upletter=1;}
 			else {flagsymbol=1;}
 		}
-		if(Downletter+flagnumber+Downletter+flagsymbol<3)
+		if(Downletter+flagnumber+Upletter+flagsymbol<3)
 		{
 			goto NG;
 		}
@@ -26,10 +26,10 @@ int main()
 			for(int j=i+3;j<s.size();j++)
 				if(s[i]==s[j]&&s[i+1]==s[j+1]&&s[i+2]==s[j+2])
 					{goto NG;}
-	OK:
-		cout<<"OK"<<endl;continue;
-	NG:
-		cout<<"NG"<<endl;
+		OK:
+			cout<<"OK"<<endl;continue;
+		NG:
+			cout<<"NG"<<endl;
 	}
 	return 0;
 	
