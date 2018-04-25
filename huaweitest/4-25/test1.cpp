ea241479d6034cf3a2816f6591b829859ee89053
wind <windspears@163.com>
2018-04-25 09:22:05 +0800

    modify a little bug

--- huaweitest/4-25/test1.cpp
+++ huaweitest/4-25/test1.cpp
@@ -13,13 +13,14 @@ char Changeletter(char ch)
 			if(ch>='a'&&ch<='c')return '2';
 			if(ch>='d'&&ch<='f')return '3';
 			if(ch>='g'&&ch<='i')return '4';
-			if(ch>='j'&&ch<='k')return '5';
+			if(ch>='j'&&ch<='l')return '5';
 			if(ch>='m'&&ch<='o')return '6';
 			if(ch>='p'&&ch<='s')return '7';
 			if(ch>='t'&&ch<='v')return '8';
 			if(ch>='w'&&ch<='z')return '9';
 		}
 	else return ch;
+	return 0;
 }
 
 int main()
