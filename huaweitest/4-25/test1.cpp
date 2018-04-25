58061b7b3c4ad92198ff2510f21944733dcf4565
wind <windspears@163.com>
2018-04-25 09:18:59 +0800

    modify a little bug

--- huaweitest/4-25/test1.cpp
+++ huaweitest/4-25/test1.cpp
@@ -7,13 +7,13 @@ char Changeletter(char ch)
 		{
 			return(ch+33);
 		}
-		else return 'A';
+		else return 'a';
 	else if(ch>='a'&&ch<='z')
 		{
 			if(ch>='a'&&ch<='c')return '2';
 			if(ch>='d'&&ch<='f')return '3';
 			if(ch>='g'&&ch<='i')return '4';
-			if(ch>='J'&&ch<='K')return '5';
+			if(ch>='j'&&ch<='k')return '5';
 			if(ch>='m'&&ch<='o')return '6';
 			if(ch>='p'&&ch<='s')return '7';
 			if(ch>='t'&&ch<='v')return '8';
@@ -27,6 +27,7 @@ int main()
 	string str;
 	while(cin>>str)
 	{
+		//cout<<str.size()<<endl;
 		for(int i=0;i<str.size();i++)
 		{
 			str[i]=Changeletter(str[i]);
