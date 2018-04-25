14f87c9d2d0d8068919b965036924869c97ae571
wind <windspears@163.com>
2018-04-25 15:31:49 +0800

    fix the input proble

--- huaweitest/4-25/test4.cpp
+++ huaweitest/4-25/test4.cpp
@@ -1,38 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-int calIncSub(vector<int> queue,vector<int> &Num)
+void calIncSub(vector<int> queue,vector<int> &Num)
 {
 	for(int i=1;i<queue.size();i++)
 		for(int j=i-1;j>=0;j--)
-		{
 			if(queue[j]<queue[i] && Num[i]<Num[j]+1)
 				Num[i]=Num[j]+1;
-		}
-	return 0;
 }
 int main()
 {
 	
 	int n,num;
-	cin>>n;
-	vector<int> queue;
-	vector<int> incNum(n,1);
-	vector<int> decNum(n,1);
-	for(int i=0;i<n;i++)
+	while(cin>>n)
 	{
-		cin>>num;
-		queue.push_back(num);
-	}
-	calIncSub(queue,incNum);
-	reverse(queue.begin(),queue.end());
-	calIncSub(queue,decNum);
-	reverse(decNum.begin(),decNum.end());
-	int max=0;
-	for(int i=0;i<n;i++)
-		if(incNum[i]+decNum[i]>max)
+		vector<int> queue;
+		vector<int> incNum(n,1);
+		vector<int> decNum(n,1);
+		for(int i=0;i<n;i++)
 		{
-			max = incNum[i]+decNum[i];
+			cin>>num;
+			queue.push_back(num);
 		}
-	cout<<n-max+1<<endl;
+		calIncSub(queue,incNum);
+		reverse(queue.begin(),queue.end());
+		calIncSub(queue,decNum);
+		reverse(decNum.begin(),decNum.end());
+		int max=0;
+		for(int i=0;i<n;i++)
+			if(incNum[i]+decNum[i]>max)
+			{
+				max = incNum[i]+decNum[i];
+			}
+		cout<<n-max+1<<endl;
+	}
 	return 0;
 }
