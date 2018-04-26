4e6f05d9640934c62645850555d3312f42ed0c4c
windspears <windspears@163.com>
2018-04-26 15:58:48 +0800

    Update and rename test2.cpp to 查找兄弟单词.cpp

--- huaweitest/4-26/查找兄弟单词.cpp
+++ huaweitest/4-26/查找兄弟单词.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 bool cmp(string str1,string str2)
 {
+//排序后比较的方法存在下述两种：酌情使用！
 	/*if(str1.length() != str2.length()) return 0;
 	int a1[128]={0},a2[128]={0};
 	int i,j;
@@ -26,6 +27,10 @@ bool cmp(string str1,string str2)
 
 int main()
 {
+//解题思路：
+//1.接收输入的字符串，sort；
+//2.循环比较，遇到同样组成但是不同序列的，记录
+//3.输出指定的字符串
 	int n;
 	while(cin>>n)
 	{
@@ -38,6 +43,7 @@ int main()
 			cin>>str;
 			vs.push_back(str);
 		}
+//对主字符串进行排序，后查找的对应编号的才是正确的！
 		sort(vs.begin(),vs.end());
 
 		cin>>word;
@@ -51,6 +57,7 @@ int main()
 					s2 = vs[i];
 			}
 		}
+	//考虑边界条件输出
 		if(!vs.empty())
 			cout<<count<<endl;
 		if(count >= index)
