4322b628197d2a376fe6b8aa8eea37fe08420677
wind <windspears@163.com>
2018-04-30 16:38:04 +0800

    test1 add

--- huaweitest/4-30/test1.cpp
+++ huaweitest/4-30/test1.cpp
@@ -1,49 +1,47 @@
 #include<bits/stdc++.h>
 using namespace std;
-void myreverse(string* s1,int src,int des)
+
+bool isaword(char ch)
 {
-	for(int i=src,j=des;i<j;i++,j--)
-	{
-		char temp;
-		temp = (*s1)[i];
-		(*s1)[i] = (*s1)[j];
-		(*s1)[j] = temp;
-	}
+	if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+		return true;
+	else 
+		return false;
 }
+
 int main()
 {
-	string s1,s2;
-	getline(cin,s1);
-    for(int i=0;i<s1.size();i++)
-    {
-        if(s1[i]>='A'&&s[i]<='Z')
-        {
-            s2[i]=s1[i];
-        }
-        else if(s1[i]>='a'&&s[i]<='z')
-        {
-            s2[i]=s1[i];
-        }
-        else if(s1[i] == ' '&&s1[i+1]!=' ')
-        {
-            s2[i]=s1[i];
-        }
-    }
-	myreverse(&s1,0,s1.size()-1);
-	int count=0;		
-	for(int i=0;i<s1.size();i++)
+	string input;
+	while(getline(cin,input))
 	{
-		if(s1[i] != ' ')
+		string temp;
+		stack<string> st1;
+		bool flag = true;
+		
+		int len = input.size();
+		for(int i=0;i<len;i++)	
 		{
-			count++;
+			if(isaword(input[i]))
+			{
+				temp+=input[i];
+				flag =true;
+			}
+			else if(!isaword(input[i])&&flag == true)
+			{
+				flag = false;
+				st1.push(temp);
+				temp.clear();
+			}
 		}
-		else if(i != s1.size()-1)
+		while(!st1.empty())
 		{
-			myreverse(&s1,i-count,i-1);
-			count=0;
+			cout<<st1.top();
+			st1.pop();
+			if(!st1.empty())
+				cout<<" ";
+			else
+				cout<<endl;
 		}
 	}
-	myreverse(&s1,s1.size()-count,s1.size()-1);
-	cout<<s1<<endl;
 	return 0;
 }
